610e2f3946f4a106ae6cfc4fb339bed68221e323
julio cesar conrado sisnero <conradojuliosisnero@gmail.com>
2025-01-26 11:47:15 -0500

    [feat] Mejorar la gestión de rutas en middleware: redirigir a home si el usuario autenticado intenta acceder a rutas públicas y verificar el token de autenticación

--- middleware.js
+++ middleware.js
@@ -1,24 +1,41 @@
 import { NextResponse } from "next/server";
+import { jwtVerify } from "jose";
 
 export async function middleware(request) {
   const token = request.cookies.get("auth-token");
   const { pathname } = request.nextUrl;
-
+  
   const publicRoutes = ["/", "/register", "/reset-password"];
+  const isPublicRoute = publicRoutes.includes(pathname);
 
-  // Si no está en ruta pública y no tiene token, redirigir a login
-  if (!publicRoutes.includes(pathname) && !token) {
+  // Si está en ruta pública y tiene token -> redirigir a home
+  if (isPublicRoute && token) {
+    return NextResponse.redirect(new URL("/home", request.url));
+  }
+
+  // Si va a ruta privada y no tiene token -> redirigir a login
+  if (!isPublicRoute && !token) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
-  // Si está en ruta pública y tiene token, redirigir a home
-  if (publicRoutes.includes(pathname) && token) {
-    return NextResponse.redirect(new URL("/home", request.url));
+  // Si tiene token, verificarlo
+  if (token) {
+    try {
+      // Obtener valor del token
+      const tokenValue = token.value;
+      const { payload } = await jwtVerify(
+        tokenValue,
+        new TextEncoder().encode(process.env.AUTH_SECRET_TOKEN)
+      );
+      return NextResponse.next();
+    } catch (error) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/", "/register", "/reset-password", "/home"],
+  matcher: ["/", "/register", "/reset-password", "/home"]
 };
